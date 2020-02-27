94706122d53c7f285e95b232f4e9a15a7d867efb
健人中井 <taketonakai@taketonakainoMacBook-Pro.local>
2020-02-27 11:33:04 +0900

    add items "reporter ID" & "union member ID"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TimePicker from './TimePicker.js'
 import SimpleBackdrop from './SimpleBackdrop.js'
 import NestedGrid from './NestedGrid.js'
 import Text from './Text.js'
-
+import InputWithIcon from './InputWithIcon.js'
 
 const houkokusakiData = [
   { key: 0, label: '米・麦・大豆'},
@@ -26,18 +26,22 @@ function App() {
     <div className="App" style={{padding: '50px 100px'}}>
         <h1>TACシステム</h1>
         <div className="topContainer">
+          <InputWithIcon label='報告者ユーザーID'/>
+          <InputWithIcon label='組合員コード'/>
           <TimePicker />
           <Text />
         </div>
-        <div className="bottomContainer" style={{marginTop: '100px'}}>
-          <div className='taskBox' styles={{marginTop: '300px'}}>
-            <NestedGrid />
-            <br/>
-            <SimpleBackdrop label='登録' />
-          </div>
-        </div>
+
     </div>
   );
 }
 
 export default App;
+
+// <div className="bottomContainer" style={{marginTop: '100px'}}>
+//   <div className='taskBox' styles={{marginTop: '300px'}}>
+//     <NestedGrid />
+//     <br/>
+//     <SimpleBackdrop label='登録' />
+//   </div>
+// </div>
---
+++ src/InputWithIcon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function InputWithIcon(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <TextField
+        className={classes.margin}
+        id="input-with-icon-textfield"
+        label={props.label}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
+}
